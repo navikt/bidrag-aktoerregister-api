5d06ba086def73bfcb4829d37248a185d1a1d5cd
Morten Hermansen <post@morher.net>
2022-08-25 17:45:29 +0200

    Initial version

---
+++ swagger/swagger-initializer.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  window.ui = SwaggerUIBundle({
+    url: "spec/bidrag-aktoerregister-api-spec.yaml",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+};
